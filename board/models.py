5cb8b0f1e3d237a8fbcce8b08b501114faf26c5e
sun007021 <sun007021@gmail.com>
2022-07-12 13:06:57 +0900

    add xss filter

--- board/models.py
+++ board/models.py
@@ -8,13 +8,13 @@ import datetime
 class CommonBoard(models.Model): #공통 게시판
     writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name='작성자')
     title = models.CharField(max_length=128, verbose_name='제목')
-    content = models.TextField(verbose_name='내용')
+    content = models.CharField(max_length=256, verbose_name='내용')
     create_date = models.DateTimeField(null=True, blank=False)
 
-class PrivateBoard1(models.Model): #공통 게시판
+class PrivateBoard1(models.Model): #비밀1 게시판
     writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name='작성자')
     title = models.CharField(max_length=128, verbose_name='제목')
-    content = models.TextField(verbose_name='내용')
+    content = models.CharField(max_length=128, verbose_name='제목')
     create_date = models.DateTimeField(null=True, blank=False)
 
 class PrivateBoard2(models.Model): #공통 게시판
--- board/templatetags/board_extras.py
+++ board/templatetags/board_extras.py
@@ -1,9 +1,18 @@
 from django import template
 from django.contrib.auth.models import Group
-
+import re
 register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
     group = Group.objects.get(name=group_name)
     return True if group in user.groups.all() else False
+
+@register.filter(name='protect_xss')
+def protect_xss(value):
+    content = value.content #전달 받은 value 객체의 content 멤버변수를 가져옴
+    #tags = value.tag_set.all() # 전달된 객체의 tag_set 전체를 가져오는 queryset을 리턴
+
+    #for tag in tags: # tags의 각각의 인스턴트 순회하며 content에서 해당 문자열을 replace
+    content = re.sub(r'script|img|noscript|javascript|SCRIPT|onload|frame', '', content)
+    return content
