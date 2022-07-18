2c0903e1825a9bfc84703db9cb6e3265d1fa084c
sun007021 <sun007021@gmail.com>
2022-07-18 14:13:03 +0900

    settings.py change DEBUG mode True to False

--- ctf_csrf문제/settings.py
+++ ctf_csrf문제/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!j$c4ju-fox8yl+71((vv#_n9mnqvu9&dp98$&wr5@@0fyh%-w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '*']
 
